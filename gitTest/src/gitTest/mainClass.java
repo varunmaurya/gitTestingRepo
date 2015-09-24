282583a9f6c9a801943b68ce6b60c64765684ee2
varunmaurya <varunmaurya@users.noreply.github.com>
2015-09-24 00:26:26 -0500

    Update mainClass.java

--- gitTest/src/gitTest/mainClass.java
+++ gitTest/src/gitTest/mainClass.java
@@ -3,6 +3,6 @@ package gitTest;
 public class mainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("Hello Git 111");
+		System.out.println("Hello Git 111 333");
 	}
 }
